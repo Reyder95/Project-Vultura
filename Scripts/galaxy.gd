94d1a41f31d8c808740c653d171934bc94efea64
Reyder95 <khoutas98@gmail.com>
2022-06-20 20:23:40 -0400

    Finalized camera - Added WASD camera movement

--- Scripts/galaxy.gd
+++ Scripts/galaxy.gd
@@ -3,4 +3,4 @@ extends Node2D
 onready var camera = get_node("Camera");
 
 func _ready():
-	camera.set_camera_bounds(-5000, 5000, -5000, 5000)
+	camera.set_camera_bounds(-5000, 5000, -5000, 5000)		# Set the camera bounds for this scene
