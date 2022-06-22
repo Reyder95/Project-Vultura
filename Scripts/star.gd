c35e662c8dd2363de098957d3e3d31ebb07acd5c
Reyder95 <khoutas98@gmail.com>
2022-06-22 01:38:29 -0400

    Started developing the signal for the button to be clicked.

--- Scripts/star.gd
+++ Scripts/star.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var star_class
 
+signal star_clicked(star)
+
 func initialize_star(star):
 	star_class = star
 	
@@ -25,3 +27,8 @@ func _on_Area2D_mouse_exited():
 	var title = get_node("Title")
 	
 	title.visible = false
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == 1 and event.is_pressed():
+		emit_signal("star_clicked", star_class)
