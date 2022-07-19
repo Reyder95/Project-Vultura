3c7478cc0eec69a6de06eaebc69492212f64a7a7
Reyder95 <khoutas98@gmail.com>
2022-07-19 14:30:29 -0400

    Built RNG maps for stars, planets, and planet resources.

--- Scripts/functions.gd
+++ Scripts/functions.gd
@@ -10,20 +10,28 @@ var planet_objects = {}
 var resource_objects = {}
 var star_objects = {}
 
+# Planet and Star RNG Tables
+var planet_rng_table = {}
+var star_rng_table = {}
+
 # Load all game data upon game start
 func _ready():
 	
+	# Parsed JSON data
 	var planet_data = {}
 	var resource_data = {}
 	var star_data = {}
 	
+	# Initial JSON files loaded
 	var planet_json = File.new()
 	var resource_json = File.new()
 	var star_json = File.new()
 	
+	# Load the planet JSON file into planet_data
 	planet_json.open("res://data/planet_data.json", File.READ)
 	planet_data = parse_json(planet_json.get_as_text())
 	
+	# Create dynamic enum, associating type with ID. Take that ID and associate it with an object
 	for planet in planet_data:
 		planet_types[parse_enum_identifier(planet.name)] = int(planet.id)
 		
@@ -41,9 +49,11 @@ func _ready():
 		
 		planet_objects[int(planet.id)] = new_planet
 	
+	# Load the resource JSON file into resource_data
 	resource_json.open("res://data/resource_data.json", File.READ)
 	resource_data = parse_json(resource_json.get_as_text())
 	
+	# Create dynamic enum, associating type with ID. Take that ID and associate it with an object
 	for resource in resource_data:
 		resource_types[parse_enum_identifier(resource.name)] = int(resource.id)
 		
@@ -55,13 +65,12 @@ func _ready():
 		}
 		
 		resource_objects[int(resource.id)] = new_resource
-		
-	for resource in planet_objects[planet_types.GAS_GIANT].resources:
-		print(resource_objects[int(resource.id)].name)
 	
+	# Load the star JSON file into star_data
 	star_json.open("res://data/star_data.json", File.READ)
 	star_data = parse_json(star_json.get_as_text())
 	
+	# Create dynamic enum, associating type with ID. Take that ID and associate it with an object
 	for star in star_data:
 		star_types[parse_enum_identifier(star.name)] = int(star.id)
 		
@@ -77,13 +86,65 @@ func _ready():
 		
 		star_objects[int(star.id)] = new_star
 	
+	# Taking all the JSON data loaded, build various RNG maps for each object's chance of spawning
+	build_rng_maps()
 	
-	
+# Each enum should be in all caps, and the spaces should instead be underscores. Example: Gas Giant -> GAS_GIANT
 func parse_enum_identifier(string):
 	var new_string = string.to_upper()
 	new_string = new_string.replace(" ", "_")
 	return new_string
 
+# Build initial RNG maps
+func build_rng_maps():
+	
+	# For each planet, build an rng map for the chance of resources spawning on the planets
+	for planet in planet_objects.values():
+		var rng_dict = {}
+		var previous_max_percent = 0
+		
+		for resource in planet.resources:
+			var current_max_percent = previous_max_percent + resource.chance
+			var new_dict = {
+				"minimum": previous_max_percent,
+				"maximum": current_max_percent
+			}
+			
+			rng_dict[int(resource.id)] = new_dict
+			previous_max_percent = current_max_percent
+		
+		planet["rng_table"] = rng_dict
+	
+	var previous_max_percent = 0
+	
+	# Build an rng map for the chance of each planet spawning in a star system
+	for planet in planet_objects.keys():
+		var current_max_percent = previous_max_percent + planet_objects[planet].chance
+		
+		var new_dict = {
+			"minimum": previous_max_percent,
+			"maximum": current_max_percent
+		}
+		
+		planet_rng_table[planet] = new_dict
+		previous_max_percent = current_max_percent
+		
+	previous_max_percent = 0
+		
+	# Build an rng map for the chance of each star spawning in the galaxy
+	for star in star_objects.keys():
+		var current_max_percent = previous_max_percent + star_objects[star].chance
+		
+		var new_dict = {
+			"minimum": previous_max_percent,
+			"maximum": current_max_percent
+		}
+		
+		star_rng_table[star] = new_dict
+		previous_max_percent = current_max_percent
+		
+		
+	
 
 # - Enums and Maps
 
